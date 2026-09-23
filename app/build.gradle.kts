c6826ac0181fbfb9288a581519d099c10673454e
olegoleg9993-hub <olegoleg9993@gmail.com>
2026-09-23 08:49:05 +0700

    Плашка поддержки виджета в две строки

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "ru.oilab.shifttimer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.2.2"
+        versionCode = 6
+        versionName = "0.2.3"
     }
 
     buildTypes {
